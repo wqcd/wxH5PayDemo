fbc00813d5ccff1265f0a303435b27e93aae1838
wxPay <37607643+baicai2@users.noreply.github.com>
2019-01-14 05:36:33 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+# -*- encoding:utf-8 -*-
+import hashlib
+import json
+import time
+import uuid
+from urllib import parse
+import re
+
+
+from tornado.httpserver import HTTPServer
+
+import tornado.web
+from tornado.ioloop import IOLoop
+
+import os
+
+import requests
+
+from tornado.options import define, parse_command_line, options
+from tornado.web import Application
+
+session = requests.session()
+
+session.verify = False
+
+def get_token(md5str):
+    # md5str = "abc"
+
+    # 生成一个md5对象
+    m1 = hashlib.md5()
+    # 使用md5对象里的update方法md5转换
+    m1.update(md5str.encode("utf-8"))
+    token = m1.hexdigest()
+    return token
+
+
+class indexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
+class Notify_URLHandler(tornado.web.RequestHandler):
+    def get(self):
+        print(self.request.body)
+        self.write("")
+
+    def post(self):
+        print(self.request.body)
+        self.write("")
+    # 微信回调函数
+
+
+
+class Notify_wxHandler(tornado.web.RequestHandler):
+    def get(self):
+
+        total_fee = "20"
+        # 金额
+
+        appid = 'appid'
+        mch_id = 'mch_id'
+        body = "微信Demo"
+        key = ''
+        host = "域名 "
+        # 微信填写的备案域名
+        nonce_str = str(uuid.uuid4()).replace('-', "")[:-3]
+        # 生成随机字符串
+        notify_url = 'http://{}/Notify_URL/'.format(host)
+        # 回调函数
+        wap_name = '腾讯充值'
+        wap_name.encode('utf-8')
+        scene_info = {
+            "h5_info": {"type": "Wap", "wap_url": "http://{}/Notify_wx".format(host), "wap_name": wap_name}}
+        out_trade_no = get_token(nonce_str)
+        # md5 加密生成订单编号
+        ip = self.request.remote_ip
+        # x_forwarded_for = self.request.META.get('HTTP_X_FORWARDED_FOR')
+        # print(self.request.META)
+        # if x_forwarded_for:
+        #     spbill_create_ip = x_forwarded_for.split(',')[0]  # 所以这里是真实的ip
+        # else:
+        #     spbill_create_ip = self.request.META.get('REMOTE_ADDR')  # 这里获得代理ip
+        # 此处 为django获取ip的方法
+
+        total_amount = str(int(float(total_fee) * 100))
+        trade_type = 'MWEB'
+        signA = "appid=%s&body=%s&mch_id=%s&nonce_str=%s&notify_url=%s&out_trade_no=%s&scene_info=%s&spbill_create_ip=%s&total_fee=%s&trade_type=%s" % (
+            appid, body, mch_id, nonce_str, notify_url, out_trade_no, scene_info, ip, total_amount,
+            trade_type)
+        print(signA)
+        strSignTmp = signA + "&key=" + key
+        sign = get_token(strSignTmp).upper()
+        # 进行MD5加密
+        print(sign)
+        post_data = "<xml>"
+        for i in (signA + "&sign=" + sign).split("&"):
+            xml1 = i.split("=")[0]
+            xml2 = i.split("=")[1]
+            post_data = post_data + '<' + xml1 + '>' + xml2 + '</' + xml1 + '>'
+        post_data = post_data + '</xml>'
+        # 组合xml请求
+        print(post_data)
+        # post_data.encode('utf-8')
+        headers = {'Content-Type': 'binary'}
+        # 解决post_data 中文编码问题
+        url = "https://api.mch.weixin.qq.com/pay/unifiedorder"
+        res = requests.post(url, data=post_data.encode(), headers=headers, verify=False)
+        # 提交订单信息
+        # res.text.encode('utf-8')
+        print(res.text.encode('latin_1').decode('utf8'))
+        pattern = re.compile("<mweb_url><!\[CDATA\[(.*?)]]></mweb_url")
+
+        redicrt_url = pattern.findall(res.text)[0]
+        # 匹配微信回调函数，调用微信app进行支付
+        self.redirect(redicrt_url)
+        # 重定向至微信
+
+
+
+
+if __name__ == '__main__':
+    # app.listen(3000)
+
+    define("port", default=8001, help="默认端口8000")
+    parse_command_line()
+    app = Application(
+        [
+            (r'/index', indexHandler),
+            (r'/Notify_wx', Notify_wxHandler),
+            (r'/Notify_URL', Notify_URLHandler),
+        ],
+
+        # 项目配置信息
+        # 网页模板
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        # 静态文件
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        # debug=False
+    )
+
+    # 部署
+    server = HTTPServer(app)
+    server.listen(options.port)
+
+    # 轮询监听
+    IOLoop.current().start()
